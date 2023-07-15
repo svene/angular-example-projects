ee254a8541fb93f8082220673a14dc897768de37
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-07-15 13:07:42 +0200

    example-app-destroyref: starting to setup DS

--- example-app-destroyref/src/app/app.component.ts
+++ example-app-destroyref/src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LeakingChildComponent} from "./leaking-child.component";
 import {SignalChildComponent} from "./signal-child.component";
+import {BaloiseDesignSystemModule} from "@baloise/design-system-components-angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LeakingChildComponent, SignalChildComponent],
-
+  imports: [CommonModule, LeakingChildComponent, SignalChildComponent, BaloiseDesignSystemModule],
+  styleUrls: ['app.component.scss'],
   template: `
-    <div class="app-header">{{title}}</div>
+    <bal-app>
+      <bal-heading level="h1" space="bottom">Heading</bal-heading>
+      <bal-card>
+        <bal-card-title>BaloiseCombi</bal-card-title>
+      </bal-card>
+      <h1 class="title is-size-xxx-large">{{title}}</h1>
     <div class="app-container">
       <div>
         <button class="app-button" type="button" (click)="toggleLeakingChild()">Toggle Leaking Child</button>
@@ -18,32 +24,12 @@ import {SignalChildComponent} from "./signal-child.component";
       <app-leaking-child *ngIf="leakingChildVisible"/>
       <app-signal-child *ngIf="signalChildVisible"/>
     </div>
+    </bal-app>
   `,
-  styles: [
-    `
-      .app-header {
-        margin: 10px 0 10px 0;
-      }
-      .app-button {
-        color: white;
-        background-color: #000d6e;
-        border-radius: 3px;
-      }
-      .app-container {
-        background-color: white;
-        border-style: dashed;
-        border-width: 1px;
-        padding: 10px;
-        border-radius: 5px;
-
-        width: 10%;
-      }
-    `
-  ],
 })
 export class AppComponent {
   title = 'example-app-destroyref';
-  leakingChildVisible = true;
+  leakingChildVisible = false;
   signalChildVisible = true;
 
   toggleLeakingChild() {
