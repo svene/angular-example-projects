dd458f73c412531497ae6772a093a481271f2626
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-07-30 05:17:10 +0200

    directly use component-store from template

--- example-app-destroyref/src/app/app.config.ts
+++ example-app-destroyref/src/app/app.config.ts
@@ -1,6 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular';
+import {provideStore} from "@ngrx/store";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(BaloiseDesignSystemModule.forRoot())],
+  providers: [
+    importProvidersFrom(BaloiseDesignSystemModule.forRoot()),
+    provideStore(),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
 };
